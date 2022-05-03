68dbcaf375da7ff80ecaa71b4354e9da69bf9c0d
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2022-05-03 10:07:23 -0400

    spring boot update

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,13 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 import java.util.List;
 import java.util.ArrayList;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
@@ -13,5 +15,10 @@ public class FakePersonDataAccessService implements PersonDao {
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
